dcb5e53862c678cbf07ce2b85600c0c58df1d9d6
Max Klimenko <ya.maxim3003@yandex.ru>
2022-03-07 18:19:19 +0300

    Add files via upload

---
+++ задание/1/first.c
@@ -0,0 +1,12 @@
+/*
+Выводит индентификатор текущего и родительского процесса
+*/
+#include <stdio.h> /* подключаю библиотеки*/
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(){
+	printf("PID дочернего процесса: %d\n",getpid());
+	printf("PID родительского процесса: %d\n",getppid());
+	return 0;
+}
